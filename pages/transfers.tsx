c2ac7d4c86da271bb92df83eb9fc6474d8e7c054
davidbarbi3r <davidbarbi3r@gmail.com>
2022-11-25 09:11:40 +0100

    :bug: Solved hydration problem

--- pages/transfers.tsx
+++ pages/transfers.tsx
@@ -60,53 +60,54 @@ export default function Transfers({}: Props) {
   return (
     <Layout>
       {show && (
-        <div className="connection">
-          {!address && (
-            <div className="container">
-              <p>
-                Please, connect your wallet to continue. Supported networks:
-                Arbitrum, Optimism, Ethereum Mainnet.{" "}
-              </p>
-              <ConnectButton />
-            </div>
+        <>
+          <div className="connection">
+            {!address && (
+              <div className="container">
+                <p>
+                  Please, connect your wallet to continue. Supported networks:
+                  Arbitrum, Optimism, Ethereum Mainnet.{" "}
+                </p>
+                <ConnectButton />
+              </div>
+            )}
+          </div>
+          {address && (
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Date </TableCell>
+                    <TableCell align="right">Amount (DAI)</TableCell>
+                    <TableCell align="right">Origin</TableCell>
+                    <TableCell align="right">Destiny</TableCell>
+                    <TableCell align="right">Fees paid (DAI)</TableCell>
+                    <TableCell align="right">Fees other Bridge (DAI)</TableCell>
+                    <TableCell align="right">Fees saved (DAI)</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {data.map((row) => (
+                    <TableRow
+                      key={row.date}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.date}
+                      </TableCell>
+                      <TableCell align="right">{row.amount}</TableCell>
+                      <TableCell align="right">{row.origin}</TableCell>
+                      <TableCell align="right">{row.destiny}</TableCell>
+                      <TableCell align="right">{row.feesPaid}</TableCell>
+                      <TableCell align="right">{row.feesOtherBridge}</TableCell>
+                      <TableCell align="right">{row.amountSaved}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
           )}
-        </div>
-      )}
-
-      {address && (
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Date </TableCell>
-                <TableCell align="right">Amount (DAI)</TableCell>
-                <TableCell align="right">Origin</TableCell>
-                <TableCell align="right">Destiny</TableCell>
-                <TableCell align="right">Fees paid (DAI)</TableCell>
-                <TableCell align="right">Fees other Bridge (DAI)</TableCell>
-                <TableCell align="right">Fees saved (DAI)</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.map((row) => (
-                <TableRow
-                  key={row.date}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.date}
-                  </TableCell>
-                  <TableCell align="right">{row.amount}</TableCell>
-                  <TableCell align="right">{row.origin}</TableCell>
-                  <TableCell align="right">{row.destiny}</TableCell>
-                  <TableCell align="right">{row.feesPaid}</TableCell>
-                  <TableCell align="right">{row.feesOtherBridge}</TableCell>
-                  <TableCell align="right">{row.amountSaved}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        </>
       )}
     </Layout>
   );
