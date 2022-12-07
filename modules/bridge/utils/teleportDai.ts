7bbb59de609b7a627c110bfe85ec994c43e17853
davidbarbi3r <davidbarbi3r@gmail.com>
2022-12-07 11:30:57 +0100

    :sparkles: adding logic & options to teleport fun

--- modules/bridge/utils/teleportDai.ts
+++ modules/bridge/utils/teleportDai.ts
@@ -1,5 +1,6 @@
 import { BigNumber, Signer } from "ethers";
 import { TeleportBridge, TeleportGUID } from "teleport-sdk";
+import { Chain, useSwitchNetwork } from "wagmi";
 
 export const approveGateway = async (
   bridge: TeleportBridge,
@@ -20,10 +21,14 @@ export const approveGateway = async (
 export const teleportDai = async (
   bridge: TeleportBridge,
   selectedAmount: BigNumber,
-  signer: Signer
+  signer: Signer,
+  //extra options (only available if useRelay)
+  maxFees?: BigNumber | undefined,
+  useRelay: boolean = true,
+  to?: string | undefined, 
+  data?: string | undefined,
 ) => {
   try {
-
     //teleport initialisation with user params
     const bridgeCall = await bridge
       .initTeleport(
@@ -33,26 +38,73 @@ export const teleportDai = async (
         signer
       )
       .then((res) => res.tx?.wait());
-    
+
     //getting tx hash after tx approved on blockchain
     const call = await bridgeCall?.transactionHash;
     console.log(call);
-    
+
     //getting all values needed to send all to a relayer
-    const guid = call && await bridge.getTeleportGuidFromTxHash(call);
-    const attestations = call && await (await bridge.getAttestations(call)).signatures;
-    const fees = await bridge.getAmounts(selectedAmount)
+    const guid = call && (await bridge.getTeleportGuidFromTxHash(call));
+    const attestations =
+      call && (await (await bridge.getAttestations(call)).signatures);
+    const fees = await bridge.getAmounts(selectedAmount);
 
-    //sending GUID and attestations to a relayer
-    await bridge.relayMintWithOracles(
-      signer,
-      guid as TeleportGUID,
-      attestations as string,
-      fees.relayFee as BigNumber
-    ).then((res) => console.log(res))
+    if (fees.relayFee && selectedAmount.lt(fees.relayFee)) {
+      useRelay = false;
+    }
 
+    if (useRelay) {
+
+      //sending GUID and attestations to a relayer (the user pay L1 gas fees in DAI to a relayer who pay L1 gas fee for him)
+      console.log("We try to use relay");
+      await bridge
+        .relayMintWithOracles(
+          signer,
+          guid as TeleportGUID,
+          attestations as string,
+          fees.relayFee as BigNumber,
+          maxFees,
+          undefined,
+          to,
+          data
+        )
+        .then((res) => console.log(res));
+
+    } else {
+
+      //bridge without relay (the user will switch to L1 & directly pay for L1 gas costs)
+      console.log("Bridging without relay")
+      await teleportDaiWithoutRelay(
+        bridge,
+        signer,
+        attestations as string,
+        guid as TeleportGUID
+      ).then((res) => console.log(res));
+    }
   } catch (error) {
-    window.alert("Ops an error occured 😭, check the console");
-    console.log(error);
+    window.alert(error);
+    console.error(error);
   }
+};
+
+const teleportDaiWithoutRelay = async (
+  bridge: TeleportBridge,
+  sender: Signer,
+  signatures: string,
+  teleportGuid: TeleportGUID,
+  maxFees?: BigNumber
+) => {
+  
+  //bridge without relay (the user will switch to L1 & directly pay for L1 gas costs)
+  
+  //change from L2 to L1 ? !! for the moment it send the tx to optimism
+
+  //call the contract 
+  await bridge.mintWithOracles(
+    teleportGuid,
+    signatures,
+    maxFees,
+    undefined,
+    sender
+  );
 };
